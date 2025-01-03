2033557a2f94001c319e384d2aeb161dca8cafff
fast-tortoise <1rn18is121@gmail.com>
2025-01-03 22:28:45 +0530

    website redirect

---
+++ content.js
@@ -0,0 +1,5 @@
+(function() {
+    if (document.location.href === 'https://www.youtube.com/' || document.location.href === 'https://www.youtube.com') {
+      window.location.replace('https://www.google.com');
+    }
+  })();
