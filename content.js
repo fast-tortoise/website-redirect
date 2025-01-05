279ecf241a9cf4159b9d0a6305f5e6d7181cbdb1
fast-tortoise <1rn18is121@gmail.com>
2025-01-05 13:40:28 +0530

    changes

--- content.js
+++ content.js
@@ -1,5 +1,8 @@
 (function() {
-    if (document.location.href === 'https://www.youtube.com/' || document.location.href === 'https://www.youtube.com') {
-      window.location.replace('https://www.google.com');
+    if (document.location.href === 'https://www.youtube.com/' ||
+        document.location.href === 'https://www.youtube.com' || 
+        document.location.href.startsWith('https://www.primevideo.com'
+        )) {
+      window.location.replace('https://www.linkedin.com/jobs/');
     }
   })();
